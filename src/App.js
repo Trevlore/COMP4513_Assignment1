689891aaf39ef3906a6260941fa1d45e57c2ec04
Farsos Bulsara <16597052+kingsukhoi@users.noreply.github.com>
2020-02-17 16:55:52 -0700

    Changed App.js to be a class component.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import './App.css';
 import Home from './Components/Home';
 
-function App() {
-  return (
-    <div className="App">
-        <Home/>
-    </div>
-  );
+class App extends React.Component {
+
+    render() {
+        return (
+            <div className="App">
+                <Home/>
+            </div>
+        );
+    }
 }
 
 export default App;
