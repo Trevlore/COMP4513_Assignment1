26691e9da4ddcb5a4a64bb94f4625834cf128752
Farsos Bulsara <16597052+kingsukhoi@users.noreply.github.com>
2020-02-17 17:01:38 -0700

    Added Movies request.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import './App.css';
 import Home from './Components/Home';
+import * as _ from 'lodash';
 
 class App extends React.Component {
+
+    state = {
+        movies: []
+    };
+
+    async componentDidMount() {
+        const request = await fetch("https://www.randyconnolly.com/funwebdev/3rd/api/movie/movies-brief.php?id=ALL");
+        const JSON = request.json();
+        const newState = await _.cloneDeep(this.state);
+        newState.movies = await JSON;
+        this.setState(newState);
+    }
 
     render() {
         return (
