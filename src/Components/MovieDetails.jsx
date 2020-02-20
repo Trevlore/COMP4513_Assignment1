43ff97c09cd104303bd8ca99d3e2264848715894
Farsos Bulsara <16597052+kingsukhoi@users.noreply.github.com>
2020-02-19 21:09:17 -0700

    removed unnecessary code

--- src/Components/MovieDetails.jsx
+++ src/Components/MovieDetails.jsx
@@ -1,31 +1,10 @@
 import React from 'react';
-import * as _ from "lodash";
-import { Link } from "react-router-dom";
 import NavBar from "./NavBar"
 import FavoritesBar from './FavoritesBar';
 
-
 class MovieDetails extends React.Component {
-
-    constructor(props) {
-        super(props);
-        this.setState({ movie: this.props.movie });
-    }
-
-    // onChange = (e) => {
-    //     const currElem = e.target;
-    //     const key = currElem.getAttribute('name');
-    //     const newState = _.cloneDeep(this.state);
-    //     newState[key] = currElem.value;
-    //     this.setState(newState);
-    // };
-
-    // style = {
-    //     height: ""
-    // };
-
+    
     render() {
-        //console.log(this.state.movie)
         return (
             <div>
                 <NavBar />
