84fc6acf0da6ae55f5e8f90d1f63a187165b4803
Farsos Bulsara <16597052+kingsukhoi@users.noreply.github.com>
2020-02-19 19:27:38 -0700

    Added About page

---
+++ src/Components/About.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+class About extends React.Component {
+    render() {
+        return(
+            <React.Fragment>
+                <ul>
+                    <li><a href="https://bulma.io/">Bulma CSS</a> </li>
+                    <li><a href="https://github.com/sghall/react-compound-slider">react-compound-slider</a> </li>
+                    <li><a href="https://reacttraining.com/react-router/web/example/basic">React Router</a> </li>
+
+                </ul>
+            </React.Fragment>
+        )
+    }
+
+}
+
+export default About
