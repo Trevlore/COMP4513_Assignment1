e095edce06aecba90955277262ddd77ae7b4c8bb
Trevlore <tbrow189@mtroyal.ca>
2020-02-21 19:51:24 -0700

    Home.jsx test transition

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,19 +1,18 @@
 import React from "react";
 import * as _ from "lodash";
 import { Link } from "react-router-dom";
-import { Redirect } from 'react-router-dom'
+import { Redirect } from 'react-router-dom';
+import { Transition, CSSTransition, TransitionGroup } from 'react-transition-group';
 
-
+import "../Style/Home.css"
 
 class Home extends React.Component {
 
-    constructor(){
+    constructor() {
         super();
-        this.state  = {searchQuery: "", searchEnter: null}; 
+        this.state = { searchQuery: "", searchEnter: null, transition: true };
         this.onKeyDown = this.onKeyDown.bind(this);
     }
-
-   
     onChange = (e) => {
         const currElem = e.target;
         const key = currElem.getAttribute('name');
@@ -37,52 +36,69 @@ class Home extends React.Component {
     };
 
 
+    animate() {
+        const newState = _.cloneDeep(this.state);
+        newState.transition = false;
+        this.setState(newState);
+    }
+
     onKeyDown(e) {
         if (e.key === 'Enter') {
-            this.setState({searchQuery: this.state.searchQuery, searchEnter : true });
+            const newState = _.cloneDeep(this.state);
+            newState.searchEnter = true;
+            this.setState(newState);
         }
     }
 
     render() {
         if (this.state.searchEnter) {
-           return this.renderKeypress();
-          }
-          else{
+            return this.renderKeypress();
+        }
+        else {
             return this.renderNormal();
-          }
+        }
 
     }
     renderKeypress() {
-        return <Redirect push to={"/movies?movie=" + this.state.searchQuery} />
+        return (
+            <Redirect push to={"/movies?movie=" + this.state.searchQuery} />
+        )
     }
 
     renderNormal() {
         return (
             <div className="is-fullheight hero">
                 <div className="column is-4 is-offset-4" style={this.style}>
-                    <div className="box" >
-                        <h3 className="title has-text-black has-text-centered">Your Movie List</h3>
+                    <CSSTransition exit={true} in={this.state.transition} classNames="searchBox" timeout={1500} appear mountOnEnter unmountOnExit>
+                        <div className="box searchBox">
+                            <h3 className="title has-text-black has-text-centered">Your Movie List</h3>
 
-                        <div className="field">
-                            <div className="control">
-                                <input name="searchQuery" onChange={this.onChange} onKeyDown={this.onKeyDown} className="input" type="text" placeholder="Title" />
+                            <div className="field">
+                                <div className="control">
+                                    <input name="searchQuery" onChange={this.onChange} onKeyDown={this.onKeyDown} className="input" type="text" placeholder="Title" />
+                                </div>
+                            </div>
+                            <div className="level">
+                                <Link to="/movies" className="level-item has-text-centered button is-block is-info">Show All Movies</Link>
+                                <Link to={"/movies?movie=" + this.state.searchQuery} className="level-item has-text-centered button is-block is-info">Search Movies</Link>
                             </div>
                         </div>
-                        <div className="level">
-                            <Link to="/movies" className="level-item has-text-centered button is-block is-info">Show All Movies</Link>
-                            <Link to={"/movies?movie=" + this.state.searchQuery} className="level-item has-text-centered button is-block is-info">Search Movies</Link>
-                        </div>
-                    </div>
+                    </CSSTransition>
+                    <button type="button" onClick={() => this.animate()}>
+                        Test Exit
+                    </button>
                 </div>
+
                 <footer>
                     <a href="https://unsplash.com/@marjan_blan" className="navbar-end" >
 
-                        <span className="tag is-dark" style={this.linkBottomRight} >Photo by</span>
+                        <span className="tag is-dark" style={this.linkBottomRight}>Photo by</span>
                         <span className="tag is-info" style={this.linkBottomLeft}>@marjanblan</span>
 
                     </a>
                 </footer>
             </div>
+
         )
     }
 }
