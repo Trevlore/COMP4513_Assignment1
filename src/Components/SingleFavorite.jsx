d4138b81223d770ab6209995f3743f182ee976d0
Farsos Bulsara <16597052+kingsukhoi@users.noreply.github.com>
2020-02-22 18:30:11 -0700

    removed unused imports and fixed react syntax

--- src/Components/SingleFavorite.jsx
+++ src/Components/SingleFavorite.jsx
@@ -1,13 +1,11 @@
 import React from "react";
-//import * as _ from "lodash";
-//import {Link} from "react-router-dom";
 
 
   class SingleFavorite extends React.Component {
    
      render() {
        return (
-         <div class="column is-1">
+         <div className="column is-1">
            <img src={"https://image.tmdb.org/t/p/w185/" + this.props.favorite.poster} alt="Sample Title" />
          </div>
        )
