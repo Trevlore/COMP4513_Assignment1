6068d31436d151498fa8a56ba7cd30ff682b016e
Farsos Bulsara <16597052+kingsukhoi@users.noreply.github.com>
2020-02-24 08:52:16 -0700

    made images smaller, and responsive to screen size.

--- src/Components/SingleMovie.jsx
+++ src/Components/SingleMovie.jsx
@@ -11,7 +11,7 @@ class SingleMovie extends React.Component {
         return (
             <tr>
                 <td>
-                    <figure className="image is-fullwidth is-2by3">
+                    <figure className="image is-fullwidth">
                         <img src={this.posterLink + this.props.imageUrl} alt="Poster"/>
                     </figure>
                 </td>
