007fd50504e89b8d8644dccc2ef42abdf3c7d240
Trevlore <tbrow189@mtroyal.ca>
2020-02-24 12:13:29 -0700

    Revert "Merge branch 'master' of https://github.com/kingsukhoi/COMP4513_Assignment1"
    This reverts commit b3f690b7b37aa0f22c05a5df5c28db09f6085aea, reversing
    changes made to 79613b9c6606df87cd429ad52c1eed5ffbd7c1b3.

--- src/App.js
+++ src/App.js
@@ -1,41 +1,31 @@
 import React from 'react';
 import './App.css';
 import Home from './Components/Home';
-import {Route, withRouter, Switch} from "react-router-dom";
-import {CSSTransition, TransitionGroup} from 'react-transition-group';
+import { Route, withRouter, Switch } from "react-router-dom";
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import MovieRoute from "./Components/MovieRoute";
 import {routeNames} from "./Helpers/RouteNames"
-import * as PropTypes from "prop-types";
+function App({ location }) {
+  return (
 
-class App extends React.Component {
-
-    render() {
-        let {location} = this.props;
-
-
-        return (
-
-            <TransitionGroup className="transition-group">
-                <CSSTransition
-                    key={location.key}
-                    timeout={{enter: 300, exit: 300}}
-                    classNames="fade"
-                >
-                    <section className="route-section">
-                        <Switch>
-                            <Route pattern={routeNames.Home.name} component={Home} exact/>
-                            <Route pattern={routeNames.Movies.name} component={MovieRoute} exact/>
-                            <Route path={routeNames.MovieDetails.name} component={MovieRoute} exact/>
-                        </Switch>
-                    </section>
-                </CSSTransition>
-            </TransitionGroup>
+    <TransitionGroup className="transition-group">
+      <CSSTransition
+        key={location.key}
+        timeout={{ enter: 300, exit: 300 }}
+        classNames="fade"
+      >
+        <section className="route-section">
+          <Switch>
+            <Route path={routeNames.Home.name} component={Home} exact />
+            <Route path={routeNames.Movies.name} component={MovieRoute} exact />
+            <Route path={routeNames.MovieDetails.name} component={MovieRoute} exact />
+          </Switch>
+        </section>
+      </CSSTransition>
+    </TransitionGroup>
 
 
-        );
-    }
+  );
 }
-
-App.propTypes = {location: PropTypes.any}
 
 export default withRouter(App);
