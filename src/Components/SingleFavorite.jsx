6e9b3ec1e4621207d8f855f950e8a690429399bc
Farsos Bulsara <16597052+kingsukhoi@users.noreply.github.com>
2020-02-24 12:18:38 -0700

    added x button on hover

--- src/Components/SingleFavorite.jsx
+++ src/Components/SingleFavorite.jsx
@@ -1,14 +1,31 @@
 import React from "react";
 import "../Style/Favorite.css"
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faWindowClose} from "@fortawesome/free-solid-svg-icons";
 
 class SingleFavorite extends React.Component {
     posterUrl = "https://image.tmdb.org/t/p/w92/";
 
+    state = {
+        hovering: false
+    };
+
+    toggleHover = (e) => {
+        this.setState(prevState => ({hovering: !prevState.hovering}));
+    };
+
     render() {
         return (
             <div className="column">
-                <figure className="image">
-                    <img className="favImage" src={this.posterUrl + this.props.favorite.url} alt={this.props.favorite.alt}/>
+                <figure className="image"
+                        onMouseEnter={this.toggleHover}
+                        onMouseLeave={this.toggleHover}
+                >
+                    {this.state.hovering ? <FontAwesomeIcon icon={faWindowClose} className="fa-2x closeButton"/> : null}
+                    <img
+                        className="favImage"
+                        src={this.posterUrl + this.props.favorite.url}
+                        alt={this.props.favorite.alt}/>
                 </figure>
             </div>
         )
