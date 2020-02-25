4dd5d83d89cbc7a4b60efa913edef8b065ec8c09
Farsos Bulsara <16597052+kingsukhoi@users.noreply.github.com>
2020-02-24 19:08:48 -0700

    added months array source

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ticket from "../Images/fa-ticket-alt.png";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 import Modal from 'react-modal';
 import "../Style/About.css"
 
@@ -17,14 +17,21 @@ class NavBar extends React.Component {
                 <Modal isOpen={this.state.showAbout} className="">
                     <header className="modal-card-head">
                         <p className="modal-card-title">About</p>
-                       
+
                     </header>
                     <section className="modal-card-body">
+                        <p>Group Members:</p>
                         <ul>
-                            <li><a href="https://bulma.io/">Bulma CSS</a> </li>
+                            <li>Farsos Bulsara</li>
+                            <li>Trevor Brown</li>
+                        </ul>
+                        <ul>
+                            <li><a href="https://bulma.io/">Bulma CSS</a></li>
                             <li><a href="https://reacttraining.com/react-router/web/example/basic">React Router</a></li>
                             <li><a href="https://www.npmjs.com/package/react-input-range">react-input-range</a></li>
                             <li><a href="https://animista.net/play/basic/slide-rotate">animista.net</a></li>
+                            <li><a href="https://css-tricks.com/everything-you-need-to-know-about-date-in-javascript/">Months
+                                array</a></li>
                         </ul>
                     </section>
                     <footer className="modal-card-foot">
