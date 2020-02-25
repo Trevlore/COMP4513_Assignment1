d688c14aad274c67e48ce8b2cf562af82e0b6c9e
Farsos Bulsara <16597052+kingsukhoi@users.noreply.github.com>
2020-02-24 19:18:18 -0700

    data for cast is showing

--- src/Components/CastView.jsx
+++ src/Components/CastView.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import "../Style/Details.css"
-import { faSync } from "@fortawesome/free-solid-svg-icons";
+import {faSync} from "@fortawesome/free-solid-svg-icons";
 
 class CastView extends React.Component {
 
@@ -10,49 +10,65 @@ class CastView extends React.Component {
 		super(props);
 	}
 
+	photoApi = "https://image.tmdb.org/t/p/w500/";
+
 	async componentDidMount() {
 		const api_key = "fef700429c99e494247e8de834e89564";
-		console.log(this.props.cast_id)
 		const request = await fetch("https://api.themoviedb.org/3/person/" + this.props.cast_id + "?api_key=" + api_key);
 		let parsedCrew = await request.json();
-		console.log(parsedCrew)
-		this.setState({ person: parsedCrew, });
+		this.setState({person: parsedCrew,});
 	}
+
+	months = [
+		'January',
+		'February',
+		'March',
+		'April',
+		'May',
+		'June',
+		'July',
+		'August',
+		'September',
+		'October',
+		'November',
+		'December'
+	];
 
 	render() {
 		// https://api.themoviedb.org/3/person/8891?api_key=api_key
 		// https://www.themoviedb.org/movie/imdb_id 
 		// https://www.imdb.com/title/imdb_id,
 		if (!this.state) {
-			return (<FontAwesomeIcon icon={faSync} className="is-text-centered fa-10x fa-spin" />)
-		}
-		else {
-			const { name } = this.state.person
+			return (<FontAwesomeIcon icon={faSync} className="is-text-centered fa-10x fa-spin"/>)
+		} else {
+			const {name, imdb_id, biography, profile_path, birthday, place_of_birth} = this.state.person;
+			const birthdayObject = new Date(birthday);
+			const birthdayString = `${this.months[birthdayObject.getMonth()]} ${birthdayObject.getDay()}, ${birthdayObject.getFullYear()}`;
 			return (
 				<div className="CastView">
-					<div class="card">
-						<div class="card-image">
-							<figure class="image is-4by3">
-								<img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image" />
+					<div className="card column">
+						<div className="card-image">
+							<figure className="image">
+								<img src={this.photoApi + profile_path} alt={name}
+									 style={{margin: "auto", height: "50vh", width: "auto"}}/>
 							</figure>
 						</div>
-						<div class="card-content">
-							<div class="media">
-								<div class="media-left">
+						<div className="card-content">
+							<div className="media">
+								<div className="media-left">
 								</div>
-								<div class="media-content">
-									<p class="title is-4">{name}</p>
-									<p class="subtitle is-6">@johnsmith</p>
+								<div className="media-content">
+									<h4 className="title is-4">{name}</h4>
+									<p className="subtitle">{`Born: ${birthdayString}, ${place_of_birth}`}</p>
+									<a href={`https://www.imdb.com/name/${imdb_id}`} className="is-link is-6">IMDB
+										Profile</a>
 								</div>
 							</div>
-
-							<div class="content">
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-      Phasellus nec iaculis mauris. <a>@bulmaio</a>.
-      <a href="#">#css</a> <a href="#">#responsive</a>
-								<time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+							<div className="media-content">
+								{biography}
 							</div>
-							<a onClick={() => this.props.castButton()} className="button is-1 is-danger is-pulled-right" style={{ margin: "0" }}>Close</a>
+							<a onClick={() => this.props.castButton()} className="button is-1 is-danger is-pulled-right"
+							   style={{margin: "0"}}>Close</a>
 						</div>
 
 					</div>
