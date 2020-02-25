28c89afeccc7898d520ea9b54421524d8ef4e7a3
Farsos Bulsara <16597052+kingsukhoi@users.noreply.github.com>
2020-02-24 17:51:36 -0700

    Collapsible FavoritesBar.jsx

--- src/Components/FavoritesBar.jsx
+++ src/Components/FavoritesBar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import SingleFavorite from "./SingleFavorite";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faCaretDown} from "@fortawesome/free-solid-svg-icons";
+import {faCaretDown, faCaretUp} from "@fortawesome/free-solid-svg-icons";
 
 
 class FavoritesBar extends React.Component {
@@ -17,32 +17,35 @@ class FavoritesBar extends React.Component {
     }
 
     style = {
-        position: "absolute",
         bottom: 0,
         marginLeft: "50%"
     };
 
     renderFavsList() {
-        return (
-            <div className="navbar-item">
-                <h2 className="has-text-grey-light">Your Favorites</h2>
-                {this.renderList()}
-            </div>
-        )
+        if (this.state.expanded)
+            return (
+                <div className="has-background-grey navbar">
+                    <div className="navbar-item">
+                        <h2 className="has-text-grey-light">Your Favorites</h2>
+                        {this.renderList()}
+                    </div>
+                </div>
+            );
+        return null
     }
 
-    toggleClose = (e)=>{
+    toggleClose = (e) => {
         this.setState({...this.state, expanded: !this.state.expanded})
     };
 
     render() {
         return (
-            <div className="has-background-grey navbar">
+            <React.Fragment>
                 {this.renderFavsList()}
                 <div style={this.style} onClick={this.toggleClose}>
-                    <FontAwesomeIcon className="fa-2x" icon={faCaretDown}/>
+                    <FontAwesomeIcon className="fa-2x" icon={this.state.expanded ? faCaretUp : faCaretDown}/>
                 </div>
-            </div>)
+            </React.Fragment>)
     }
 
 
